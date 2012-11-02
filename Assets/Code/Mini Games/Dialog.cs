93926614f8aaf25120de0b508654f10d222d6706
Isaac James <izach.james@gmail.com>
2012-11-02 14:39:08 -0700

    Made Dialog pretty
    Unity also recompiled some stuff

--- Assets/Code/Mini Games/Dialog.cs
+++ Assets/Code/Mini Games/Dialog.cs
@@ -19,15 +19,33 @@ public class Dialog : MonoBehaviour, MiniGameAPI.IMiniGame {
 		if (data == null) {
 			return;
 		}
+		GUILayout.BeginArea(new Rect(0f, 0f, Screen.width, 20f));
 		foreach (string promptLine in promptText()) {
 			GUILayout.Label(promptLine);
 		}
-		foreach (XmlNode xn in data.SelectNodes("descendant::response")) {
+		GUILayout.EndArea();
+		XmlNodeList options = data.SelectNodes("descendant::response");
+		int width;
+		for (
+			width = (int)Mathf.Sqrt(options.Count);
+			width > 0 && width * (int)(((float)options.Count) / width) != options.Count;
+			width++
+		);
+		GUILayout.BeginArea(new Rect(0f, 20f, Screen.width, Screen.height - 20f));
+		int cur = 0;
+		foreach (XmlNode xn in options) {
+			if (cur % width == 0) {
+				GUILayout.BeginHorizontal();
+			}
 			GUI.enabled = Requirements.pass(xn);
-			if (GUILayout.Button(XmlUtilities.getData(xn))) {
+			if (GUILayout.Button(XmlUtilities.getData(xn), GUILayout.ExpandHeight(true))) {
 				MiniGameController.endMiniGame(xn.Attributes[XmlUtilities.edge].Value);
 			}
 			GUI.enabled = true;
+			if (++cur % width == 0) {
+				GUILayout.EndHorizontal();
+			}
 		}
+		GUILayout.EndArea();
 	}
 }
