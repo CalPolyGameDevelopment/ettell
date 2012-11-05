ae99435810f81add9996575cbfb0ac92dec1243e
Dillon Hicks <chronodynamic@gmail.com>
2012-11-05 01:10:24 -0600

    The scene and code for the music creator demo prototype.

---
+++ Assets/Code/Mechanics/MusicCreatorButtons.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class MusicCreatorButtons : MonoBehaviour {
+ 
+    void OnGUI () {
+     if (GUI.Button(new Rect (10,10,150,100), "Accept")) {
+         
+     }
+ }
+
+}
---
+++ Assets/Code/Mechanics/PowerBar.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class PowerBar : MonoBehaviour {
+
+    public float power = 50;
+    public int maxPower = 100;
+    public float powerIncrement = 10;
+    
+    public Color backgroundColor = Color.black;
+    public Color forgroundColor = Color.red;
+    
+    
+    public Rect box = new Rect(10, 10, 400, 20);
+
+    private Texture2D background;
+    private Texture2D foreground;
+    void Start()
+    {
+
+        background = new Texture2D(1, 1, TextureFormat.RGB24, false);
+        foreground = new Texture2D(1, 1, TextureFormat.RGB24, false);
+
+        background.SetPixel(0, 0, Color.black);
+        foreground.SetPixel(0, 0, Color.red);
+        
+        background.Apply();
+        foreground.Apply();
+    }
+
+
+    void OnGUI()
+    {
+        GUI.BeginGroup(box);
+        {
+            
+            GUI.DrawTexture(new Rect(0, 0, box.width, box.height), background, ScaleMode.StretchToFill);
+            GUI.DrawTexture(new Rect(0, 0, box.width*power/maxPower, box.height), foreground, ScaleMode.StretchToFill);
+
+                 }
+        GUI.EndGroup(); ;
+    }
+ 
+    
+    void AddPower(){
+       power += powerIncrement;
+    }
+}
