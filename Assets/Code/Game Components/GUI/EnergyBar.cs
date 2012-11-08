93723a75c418dad819be9e25688d22c42af2a947
Dillon Hicks <chronodynamic@gmail.com>
2012-11-08 00:38:58 -0600

    rearranging the code directory, I think it is a bit better now.

---
+++ Assets/Code/Game Components/GUI/EnergyBar.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnergyBar : MonoBehaviour {
+
+    public float energy = 0;
+    public int maxEnergy = 100;
+    public float energyIncrement = 10;
+    
+    public Color backgroundColor = Color.black;
+    public Color forgroundColor = Color.red;
+
+   
+    
+    public Rect box = new Rect(10, 10, 400, 20);
+
+    private Texture2D background;
+    private Texture2D foreground;
+    
+    void Start()
+    {
+
+        background = new Texture2D(1, 1, TextureFormat.RGB24, false);
+        foreground = new Texture2D(1, 1, TextureFormat.RGB24, false);
+
+        background.SetPixel(0, 0, backgroundColor);
+        foreground.SetPixel(0, 0, forgroundColor);
+        
+        background.Apply();
+        foreground.Apply();
+    }
+
+
+    void OnGUI()
+    {
+        GUI.BeginGroup(box);
+        {
+            
+            GUI.DrawTexture(new Rect(0, 0, box.width, box.height), background, ScaleMode.StretchToFill);
+            GUI.DrawTexture(new Rect(0, 0, box.width*energy/maxEnergy, box.height), foreground, ScaleMode.StretchToFill);
+
+                 }
+        GUI.EndGroup(); ;
+    }
+ 
+    public float Value {
+        get{
+            return energy;
+        }
+        set{
+            // Make sure energy can't go over max or under min?
+            energy = value;
+        }
+    }
+    
+    
+    public void DecrementEnergy(){
+        
+        energy -= energyIncrement;
+        
+    }
+    
+    public void IncrementEnergy(){
+        
+          energy += energyIncrement;
+    }
+}
