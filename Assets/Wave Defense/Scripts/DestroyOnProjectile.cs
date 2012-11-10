b3437c1dadf8320312653649217eff8044f140f2
Dillon Hicks <chronodynamic@gmail.com>
2012-11-09 23:39:06 -0600

    A script to destroy a gameobject when it gets hit by a projectile.

--- Assets/Wave Defense/Scripts/DestroyOnProjectile.cs
+++ Assets/Wave Defense/Scripts/DestroyOnProjectile.cs
@@ -5,9 +5,13 @@ public class DestroyOnProjectile : MonoBehaviour {
  
     const string PROJECTILE_TAG = "Projectile";
     
+    public bool destroyesProjectile = true;
+    
     void OnTriggerEnter(Collider other){
         if (!other.gameObject.CompareTag(PROJECTILE_TAG))
             return;
+        if (destroyesProjectile)
+            Destroy(other.gameObject);
         
         Destroy(gameObject);
         
