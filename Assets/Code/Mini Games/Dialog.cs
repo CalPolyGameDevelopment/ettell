f7f672ce509b3cfcb8447a05cc1902f1c43498b3
Isaac James <izach.james@gmail.com>
2012-11-14 13:18:04 -0800

    Update Dialog
    Now functions as a invisible choice node, makes smarter decisions about
    rows (i.e. 10 buttons is now 3,3,4 instead of 5,5.  15 is 4,4,4,3
    instead of 5,5,5).  Uses monospace font.

--- Assets/Code/Mini Games/Dialog.cs
+++ Assets/Code/Mini Games/Dialog.cs
@@ -1,18 +1,55 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 
 public class Dialog : MonoBehaviour, MiniGameAPI.IMiniGame {
+	
+	//public Font font;
+	
+	private const string PROMPT = "prompt";
+	private const string RESPONSE = "response";
+	private const int BUTTON_SIZE_MODIFIER = 30;
+	
+	public GUIStyle style;
+	private XmlNode[][] options;
+	private string[] promptText;
 	
 	private XmlNode data;
 	public XmlNode Data {
 		set {
 			data = value;
+			XmlNode[] possibleOptions = data.SelectNodes(RESPONSE).Cast<XmlNode>().Where(xn => Requirements.pass(xn)).ToArray();
+			promptText = XmlUtilities.getDataFromNode<string>(data, PROMPT, XmlUtilities.getData).ToArray();
+			if (possibleOptions.Length == 1) {
+				MiniGameController.endMiniGame(XmlUtilities.getData(possibleOptions[0]));
+			}
+			
+			//style = new GUIStyle();
+			//style.font = font;
+			//style.alignment = TextAnchor.MiddleCenter;
+			//style.
+			
+			int height = Mathf.RoundToInt(Mathf.Sqrt((float)possibleOptions.Length));
+			HashSet<XmlNode>[] rows = new HashSet<XmlNode>[height];
+			for (int i = 0; i < height; i++) {
+				rows[i] = new HashSet<XmlNode>();
+			}
+			// This is a greedy approximation to the knapsack problem.  Trying to equalize the number of characters per line
+			foreach (XmlNode xn in possibleOptions.OrderBy<XmlNode, int>(xn => -XmlUtilities.getData(xn).Length)) {
+				rows[0].Add(xn);
+				rows = rows.OrderBy<HashSet<XmlNode>, int>(
+					r => r.Aggregate<XmlNode, int>(0, 
+						(agg, xml) => agg + XmlUtilities.getData(xml).Length + BUTTON_SIZE_MODIFIER
+					)
+				).ToArray();
+			}
+			
+			options = new XmlNode[height][];
+			for (int i = 0; i < height; i++) {
+				options[i] = rows[i].ToArray();
+			}
 		}
-	}
-	
-	protected IEnumerable<string> promptText() {
-		return XmlUtilities.getDataFromNode<string>(data, "descendant::prompt", XmlUtilities.getData);
 	}
 	
 	void OnGUI() {
@@ -20,31 +57,19 @@ public class Dialog : MonoBehaviour, MiniGameAPI.IMiniGame {
 			return;
 		}
 		GUILayout.BeginArea(new Rect(0f, 0f, Screen.width, 20f));
-		foreach (string promptLine in promptText()) {
+		foreach (string promptLine in promptText) {
 			GUILayout.Label(promptLine);
 		}
 		GUILayout.EndArea();
-		XmlNodeList options = data.SelectNodes("descendant::response");
-		int width;
-		for (
-			width = (int)Mathf.Sqrt(options.Count);
-			width > 0 && width * (int)(((float)options.Count) / width) != options.Count;
-			width++
-		);
 		GUILayout.BeginArea(new Rect(0f, 20f, Screen.width, Screen.height - 20f));
-		int cur = 0;
-		foreach (XmlNode xn in options) {
-			if (cur % width == 0) {
-				GUILayout.BeginHorizontal();
+		foreach (XmlNode[] xns in options) {
+			GUILayout.BeginHorizontal();
+			foreach (XmlNode xn in xns) {
+				if (GUILayout.Button(XmlUtilities.getData(xn), style, GUILayout.ExpandHeight(true))) {
+					MiniGameController.endMiniGame(xn.Attributes[XmlUtilities.edge].Value);
+				}
 			}
-			GUI.enabled = Requirements.pass(xn);
-			if (GUILayout.Button(XmlUtilities.getData(xn), GUILayout.ExpandHeight(true))) {
-				MiniGameController.endMiniGame(xn.Attributes[XmlUtilities.edge].Value);
-			}
-			GUI.enabled = true;
-			if (++cur % width == 0) {
-				GUILayout.EndHorizontal();
-			}
+			GUILayout.EndHorizontal();
 		}
 		GUILayout.EndArea();
 	}
