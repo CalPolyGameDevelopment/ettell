3cc82048e195733700719342eaf853cbab8b2d52
Isaac James <izach.james@gmail.com>
2012-11-14 10:48:09 -0800

    Fix BC bars
    They were only getting half full when the sln length is 2.  I ended up
    stripping out all the message passing things to them too, since there
    was a comment saying it was needed.

--- Assets/Bulls And Cleaots/Controllers/SolutionMixBar.cs
+++ Assets/Bulls And Cleaots/Controllers/SolutionMixBar.cs
@@ -1,42 +1,16 @@
 using UnityEngine;
 using System.Collections;
 
-public class SolutionMixEvent : IEvent {
-    private string name;
-    int digitCount;
-
-    public SolutionMixEvent(int count) {
-        name = this.GetType().ToString();
-        digitCount = count;
-    }
-
-
-    string IEvent.GetName() {
-        return name;
-    }
-    object IEvent.GetData() {
-        return digitCount;
-    }
-}
-
 [RequireComponent(typeof(EnergyBar))]
-public class SolutionMixBar : MonoBehaviour, IEventListener {
+public class SolutionMixBar : MonoBehaviour {
 
     void Start() {
         GetComponent<EnergyBar>().labelText = "Solution Mix";
-        EventManager.instance.RegisterListener(this, typeof(SolutionMixEvent).ToString());
     }
 
-    public bool HandleEvent(IEvent evnt) {
-        SetSolutionMix((int)evnt.GetData());
-        return true;
-    }
-
-    public void SetSolutionMix(int numFound) {
-
+    public void FoundSolution(float barFullness) {
         EnergyBar energyBar = gameObject.GetComponent<EnergyBar>();
-        energyBar.Value = energyBar.energyIncrement * numFound;
+        energyBar.Fullness = barFullness;
     }
-
 }
 
