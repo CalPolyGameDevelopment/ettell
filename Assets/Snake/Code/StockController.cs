706b6709d295e83e7ef3fdb9b9b4743d18fc2102
Isaac James <izach.james@gmail.com>
2012-11-16 16:12:27 -0800

    Remove bad stocks after a while

--- Assets/Snake/Code/StockController.cs
+++ Assets/Snake/Code/StockController.cs
@@ -29,6 +29,12 @@ public class StockController : MonoBehaviour {
 	}
 	
 	void checkSymbols() {
+		foreach (Symbol s in symbols) {
+			if (SnakeGame.Singleton[s.x, s.y] == SnakeGame.Singleton.BadStock && Random.Range(0f, 1f) < badStockDiffusionChance) {
+				SnakeGame.Singleton[s.x, s.y] = SnakeGame.EMPTY_COLOR;
+			}
+		}
+		// This will remove symbols that have been eaten or diffused from the ticker
 		symbols = symbols.Where(s => 
 			SnakeGame.Singleton[s.x, s.y] == SnakeGame.Singleton.BadStock || 
 			SnakeGame.Singleton[s.x, s.y] == SnakeGame.Singleton.GoodStock
