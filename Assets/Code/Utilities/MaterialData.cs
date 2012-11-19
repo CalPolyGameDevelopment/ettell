72faa1c4aa1aba101b8017ff50d2c0f7f6963112
Dillon Hicks <chronodynamic@gmail.com>
2012-11-18 18:30:55 -0600

    Nuking undeeded color parsing code.

--- Assets/Code/Utilities/MaterialData.cs
+++ Assets/Code/Utilities/MaterialData.cs
@@ -37,6 +37,7 @@ public static class MaterialData {
             select GetColor(cn);
         return parsedColors.ToArray();
     }
+
 }
 
 
@@ -45,19 +46,6 @@ public class UnparsableColorException : Exception
     public UnparsableColorException (string message) : base(message)
     {
     }    
-}
-
-static class ColorNames
-{
-    public const string Red = "red";
-    public const string Green = "green";
-    public const string Blue = "blue";
-    public const string Black = "black";
-    public const string Cyan = "cyan";
-    public const string Magenta = "magenta";
-    public const string White = "white";
-    public const string Yellow = "yellow";
-    public const string Alpha = "alpha";
 }
 
 
@@ -78,29 +66,30 @@ static class ColorUtilities
 	
 	public static string COLOR = "color";
 	
+    public const string Red = "red";
+    public const string Green = "green";
+    public const string Blue = "blue";
+    public const string Alpha = "alpha";
+    
     // The hex value for the maximum value of a component of a color 
     // used for converting from the normal color values to the 
     // float values used by Unity.
     public const int RGBA_COMPONENT_MAX = 0xff;  // = 255 = 2^8 - 1 = b11111111 = b8_7777 
  
     // For symmetry and readability.
-    public const int RGBA_COMPONENT_MIN = 0;  // Duh. 
- 
-    public const string HTML_HEX_ESCAPE = "#";
+    public const int RGBA_COMPONENT_MIN = 0;  // Duh.  
     public const string HTML_RGB_ESCAPE = "rgb";
     
-    // Note that HTML hex codes have no alpha.
-    // Matches "#xxxxxx" where the x's are hex values 00-ff
-    public const string HTML_HEX_PATTERN = @"
-                         [#]  # The escape
-         (?<red>[0-9a-f]{2})  # 2 hex digits for red
-       (?<green>[0-9a-f]{2})  # 2 hex digits for green
-        (?<blue>[0-9a-f]{2})  # 2 hex digits for blue
-       (?<alpha>[0-9a-f]{2})? # opt. 2 hex digigts for alpha
-    ";
-    public static Regex HtmlHexRegex = new Regex (HTML_HEX_PATTERN,
-        RegexOptions.IgnoreCase |
-        RegexOptions.IgnorePatternWhitespace);
+    public static List<Color> namedColors = new List<Color>{
+        Color.black,
+        Color.blue,
+        Color.gray,
+        Color.magenta,
+        Color.red,
+        Color.white,
+        Color.yellow
+    };
+
     
     // Matches "rgb(xxx,xxx,xxx)" where the x's are 0-255
     // also will match "rgb(xxx,xxx,xxx,xxx)" which includes
@@ -117,30 +106,6 @@ static class ColorUtilities
         RegexOptions.IgnoreCase |
         RegexOptions.IgnorePatternWhitespace);
     
-    // Open Question: Should we use consts for color names?
-    public static readonly Dictionary<string,Color> nameToValueMap = 
-        new Dictionary<string, Color>{
-            {ColorNames.Red, Color.red},
-            {ColorNames.Green, Color.green},
-            {ColorNames.Blue, Color.blue},
-            {ColorNames.Black, Color.black},
-            {ColorNames.Cyan, Color.cyan},
-            {ColorNames.Magenta, Color.magenta},
-            {ColorNames.White, Color.white},
-            {ColorNames.Yellow, Color.yellow},
-    };
-    public static readonly Dictionary<Color,string> valueToNameMap =
-     new Dictionary<Color,string>{
-         {Color.red, ColorNames.Red},
-         {Color.green, ColorNames.Green},
-         {Color.blue, ColorNames.Blue},
-         {Color.black, ColorNames.Black},
-         {Color.cyan, ColorNames.Cyan},
-         {Color.magenta, ColorNames.Magenta},
-         {Color.white, ColorNames.White},
-         {Color.yellow, ColorNames.Yellow},    
-    };
-   
 	#endregion
     
     // Simply convert normal integer value for RGB [0,255] to the unity 
@@ -160,21 +125,14 @@ static class ColorUtilities
         return (float)hex / RGBA_COMPONENT_MAX;
     }
     
-    public static Color FromName (string name)
-    {
-        return nameToValueMap [name];
-    }
- 
-    public static string GetName (Color color)
-    {
-        return valueToNameMap [color];
-    }
-    
 
 	#region Parse Functions
 
     public static IEnumerable<Color> GetColors(){
-        return nameToValueMap.Values;
+        
+        foreach (Color c in namedColors){
+            yield return c;
+        }
         
     }
     
@@ -203,16 +161,13 @@ static class ColorUtilities
         }
     }
     
-    // Parse html style colors #xxxxxx and rgb(xxx,...)
+    // Parse html style rgb(xxx,...)
     public static Color Parse (string colorCode)
     {     
         colorCode = colorCode.ToLower ();
-        
-        if (colorCode.StartsWith (HTML_HEX_ESCAPE)) {
-            return ParseHtmlHex (colorCode);
-        } else if (colorCode.StartsWith (HTML_RGB_ESCAPE)) {
-            return ParseHtmlRGB (colorCode);
-        }     
+        if (colorCode.StartsWith (HTML_RGB_ESCAPE)) {
+            return ParseHtmlRGB(colorCode);
+        }
         throw new UnparsableColorException ("Is this in HTML format?: " + colorCode);
         
     }
@@ -229,10 +184,8 @@ static class ColorUtilities
         int gPrime = int.Parse(g);
         int bPrime = int.Parse(b);
         int aPrime = int.Parse(a);
-
-        
-        return Parse (rPrime, gPrime, bPrime, aPrime);
-        
+  
+        return Parse (rPrime, gPrime, bPrime, aPrime);        
     }
     
     // 0-255 integer string for RGB
@@ -245,58 +198,24 @@ static class ColorUtilities
         return Parse (rPrime, gPrime, bPrime, RGBA_COMPONENT_MAX);
         
     }
-    
-    // Parse from RGB hex string "00"-"FF"
-    private static Color ParseHex (string r, string g, string b, string a)
-    {
-        int rPrime = int.Parse (r, NumberStyles.HexNumber);
-        int gPrime = int.Parse (g, NumberStyles.HexNumber);
-        int bPrime = int.Parse (b, NumberStyles.HexNumber);
-        int aPrime = int.Parse (a, NumberStyles.HexNumber);
 
-        return Parse (rPrime, gPrime, bPrime, aPrime);
-    }
-    
-    // Get the unity color from the "#XXXXXXXX" string.
-    private static Color ParseHtmlHex (string htmlCode)
-    {
-        
-        Match hexMatch = HtmlHexRegex.Match (htmlCode);       
 
-        string r, g, b, a = "ff";
-
-        
-        
-        if (!hexMatch.Success) {
-            throw new UnparsableColorException (
-                "Is this in the HTML hex format?: " + htmlCode.ToString ());
-        }
-        
-        r = hexMatch.Groups [ColorNames.Red].Value;
-        g = hexMatch.Groups [ColorNames.Green].Value;
-        b = hexMatch.Groups [ColorNames.Blue].Value;
-        if(hexMatch.Groups[ColorNames.Alpha].Success){
-            a = hexMatch.Groups[ColorNames.Alpha].Value;
-        }
-        return ParseHex (r, g, b, a);
-    }
-    
     private static Color ParseHtmlRGB (string htmlCode)
     {
         Match rgbMatch = HtmlRGBRegex.Match (htmlCode);
         string r, g, b, a = "255";
         
-        if (!rgbMatch.Groups [ColorNames.Red].Success) {
+        if (!rgbMatch.Groups [Red].Success) {
             throw new UnparsableColorException (
                 "Is this in the HTML hex format?: " + htmlCode.ToString ());
         }
        
-        r = rgbMatch.Groups [ColorNames.Red].Value;
-        g = rgbMatch.Groups [ColorNames.Green].Value;
-        b = rgbMatch.Groups [ColorNames.Blue].Value;
+        r = rgbMatch.Groups [Red].Value;
+        g = rgbMatch.Groups [Green].Value;
+        b = rgbMatch.Groups [Blue].Value;
         
-        if (rgbMatch.Groups [ColorNames.Alpha].Success){
-            a = rgbMatch.Groups [ColorNames.Alpha].Value;    
+        if (rgbMatch.Groups [Alpha].Success){
+            a = rgbMatch.Groups [Alpha].Value;    
         }
         
         return Parse (r, g, b, a);
