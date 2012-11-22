e9561158f70c3005566d5599a8ed2f5e2270f469
Dillon Hicks <chronodynamic@gmail.com>
2012-11-22 10:35:13 -0600

    Formatting to get rid of spaces/tab git debacle.

--- Assets/Bulls And Cleaots/Controllers/BullsAndCleotsLevelController.cs
+++ Assets/Bulls And Cleaots/Controllers/BullsAndCleotsLevelController.cs
@@ -3,66 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class BCLevelData {
-    public SolutionManager slnManager;
-    public List<List<Material>> choices;
+public class BCLevelData
+{
+	public SolutionManager slnManager;
+	public List<List<Material>> choices;
 
+	public BCLevelData (SolutionManager mgr, List<List<Material>> chcs)
+	{
+		slnManager = mgr;
+		choices = chcs;
+	}
 
-    public BCLevelData(SolutionManager mgr, List<List<Material>> chcs){
-        slnManager = mgr;
-        choices = chcs;
-    }
+	public SolutionManager SolutionManager {
+		get{ return slnManager;}
+	}
 
-
-    public SolutionManager SolutionManager{
-         get{ return slnManager;}
-    }
-    public List<List<Material>> Choices{
-         get{ return choices; }
-    }
+	public List<List<Material>> Choices {
+		get{ return choices; }
+	}
 }
 
-
-public class BullsAndCleotsLevelController : MonoBehaviour, IEventListener {
+public class BullsAndCleotsLevelController : MonoBehaviour, IEventListener
+{
 	
 	// State and Init Data
 	private BCLevelData initData = null;
-    public BCLevelData InitData{
+
+	public BCLevelData InitData {
 		get {
-			if( initData != null ){
+			if (initData != null) {
 				return initData;
 			}
-			throw new System.NullReferenceException(
+			throw new System.NullReferenceException (
 				"Bulls and Cleots level data has not been initialized!");
 		}
-        set {
-            if (initData != null){
-                Debug.LogWarning("Level data already set!");
-                return;
-            }
-            initData = value;
-        }
-    }
+		set {
+			if (initData != null) {
+				Debug.LogWarning ("Level data already set!");
+				return;
+			}
+			initData = value;
+		}
+	}
 	
 	private bool hasWon;
 	private int attemptCount;
-    public SolutionManager slnManager;
+	public SolutionManager slnManager;
 	
 	// Player interaction objects
-    public GameObject inputPane;
-    public GameObject testBlocks;
+	public GameObject inputPane;
+	public GameObject testBlocks;
     
 	// GUI/HUD 
 	public EnergyBarManager barManager;
 	public Rect attemptButtonRect;	
 	
 	// Events
-    static string onSnapName = typeof(DraggableOnSnapEvent).ToString();
-    static string snapOnEnterName = typeof(SnappableOnEnterEvent).ToString();
-    static string snapOnExitName = typeof(SnappableOnExitEvent).ToString();
-    static string onMoveName = typeof(DraggableOnMoveEvent).ToString();
- 
-    private string[] handledEventNames = {
+	static string onSnapName = typeof(DraggableOnSnapEvent).ToString ();
+	static string snapOnEnterName = typeof(SnappableOnEnterEvent).ToString ();
+	static string snapOnExitName = typeof(SnappableOnExitEvent).ToString ();
+	static string onMoveName = typeof(DraggableOnMoveEvent).ToString ();
+	private string[] handledEventNames = {
         onSnapName,
         onMoveName,
         snapOnEnterName,
@@ -72,85 +73,90 @@ public class BullsAndCleotsLevelController : MonoBehaviour, IEventListener {
  
 	#region Init Methods
 	
-	private void initStateData(){
+	private void initStateData ()
+	{
 		hasWon = false;
-        attemptCount = 0;
+		attemptCount = 0;
 		slnManager = initData.SolutionManager;
 	}
 	
-	private void initPlayerPieces(){
-        testBlocks = Instantiate(testBlocks) as GameObject;
+	private void initPlayerPieces ()
+	{
+		testBlocks = Instantiate (testBlocks) as GameObject;
 
-        testBlocks.transform.parent = transform;
-		List<Material> allMaterials = new List<Material>();
+		testBlocks.transform.parent = transform;
+		List<Material> allMaterials = new List<Material> ();
 		
 		// Look up Aggregate/Accumulate for this instead.
-        foreach(List<Material> mats in initData.Choices){
+		foreach (List<Material> mats in initData.Choices) {
             
-            allMaterials.AddRange(mats);
-        }
+			allMaterials.AddRange (mats);
+		}
 		
-		testBlocks.GetComponent<SolutionBlocks>().Choices = allMaterials.ToArray();
+		testBlocks.GetComponent<SolutionBlocks> ().Choices = allMaterials.ToArray ();
 		
-        inputPane = Instantiate(inputPane) as GameObject;
-        inputPane.GetComponent<SolutionInputPanel>().solutionLength = slnManager.Length;
-        inputPane.transform.parent = transform;
+		inputPane = Instantiate (inputPane) as GameObject;
+		inputPane.GetComponent<SolutionInputPanel> ().solutionLength = slnManager.Length;
+		inputPane.transform.parent = transform;
 	}
 	
-	private void initGUI(){
-		GameObject gui = new GameObject("Bulls and Cleots GUI");
+	private void initGUI ()
+	{
+		GameObject gui = new GameObject ("Bulls and Cleots GUI");
 		gui.transform.parent = transform;
-		barManager = gui.AddComponent<EnergyBarManager>();
+		barManager = gui.AddComponent<EnergyBarManager> ();
 		barManager.slnCount = slnManager.Count;
 		barManager.slnLength = slnManager.Length;
 	}
 	
-	private void initEvents(){
+	private void initEvents ()
+	{
 		// Set the instance of the event manager to be a child of
 		// the current scene so it gets cleaned up properly.
 		EventManager.instance.transform.parent = transform;
 		
-        // register as listener for desired events
-        foreach (string eventName in handledEventNames) {
-            EventManager.instance.RegisterListener(this, eventName);
-        }
+		// register as listener for desired events
+		foreach (string eventName in handledEventNames) {
+			EventManager.instance.RegisterListener (this, eventName);
+		}
 	}
-	void Start() {
 
-        initStateData();
-		initPlayerPieces();
-		initGUI();
-		initEvents();
-     
-    }
+	void Start ()
+	{
+		initStateData ();
+		initPlayerPieces ();
+		initGUI ();
+		initEvents (); 
+	}
 	
 	#endregion
 	
 
-	void OnGUI() {
+	void OnGUI ()
+	{
 
-        if(!hasWon && GUI.Button(attemptButtonRect, "Attempt")) {
-        	PlayerAttemptSolve();
-        }
-        else if (hasWon && GUI.Button(attemptButtonRect, "You Win!")) {
-            PlayerEndGame();
-        }
+		if (!hasWon && GUI.Button (attemptButtonRect, "Attempt")) {
+			PlayerAttemptSolve ();
+		} else if (hasWon && GUI.Button (attemptButtonRect, "You Win!")) {
+			PlayerEndGame ();
+		}
 
-    }
+	}
 	
 	/// <summary>
 	/// Updates the Player Bulls and Cleots hint bars.
 	/// </summary>
-	void doGUIAccounting(){
+	void doGUIAccounting ()
+	{
 		
-		int[] perSlnBulls = slnManager.GetBullsCount();
-		int[] perSlnCleots = slnManager.GetCleotsCount();
-		foreach(int index in Enumerable.Range(0, slnManager.Count)){
-			int cleots = perSlnCleots[index];
-			int bulls = perSlnBulls[index];
+		int[] perSlnBulls = slnManager.GetBullsCount ();
+		int[] perSlnCleots = slnManager.GetCleotsCount ();
+		foreach (int index in Enumerable.Range(0, slnManager.Count)) {
+			int cleots = perSlnCleots [index];
+			int bulls = perSlnBulls [index];
 			
-			barManager.SetBulls(index, bulls);
-			barManager.SetCleots(index, cleots);
+			barManager.SetBulls (index, bulls);
+			barManager.SetCleots (index, cleots);
 		}
 		
 	}
@@ -159,29 +165,29 @@ public class BullsAndCleotsLevelController : MonoBehaviour, IEventListener {
 	/// <summary>
 	/// Called when the player hits the "Attempt" button.
 	/// </summary>
-    void PlayerAttemptSolve() {
+	void PlayerAttemptSolve ()
+	{
 		
-		if(slnManager.HasBlankGuesses){
+		if (slnManager.HasBlankGuesses) {
 			return;
 		}
 		
 		attemptCount++;
 		
-		doGUIAccounting();
+		doGUIAccounting ();
 		
-		if(slnManager.Solved){
+		if (slnManager.Solved) {
 			hasWon = true;
 			return;
 		}
 		
-		ResetGuesses();
+		ResetGuesses ();
 	}
 
+	void PlayerEndGame ()
+	{
 
-
-    void PlayerEndGame() {
-
-		MiniGameController.endMiniGame(
+		MiniGameController.endMiniGame (
         	BullsAndCleots.ExitEdges.IncreaseDifficulty);
 
 	}
@@ -189,104 +195,103 @@ public class BullsAndCleotsLevelController : MonoBehaviour, IEventListener {
     
     
    
-    /// <summary>
-    ///  Accounting for a block being dropped to the guess pane.
-    /// </summary>
-	void GuessDropped(GameObject obj) {
-        Draggable draggable = obj.GetComponent<Draggable>();
-        Snappable snappable = draggable.currentSnappable.GetComponent<Snappable>();
+	/// <summary>
+	///  Accounting for a block being dropped to the guess pane.
+	/// </summary>
+	void GuessDropped (GameObject obj)
+	{
+		Draggable draggable = obj.GetComponent<Draggable> ();
+		Snappable snappable = draggable.currentSnappable.GetComponent<Snappable> ();
 
-        SolutionBlock solutionBlock = obj.GetComponent<SolutionBlock>();
-        SolutionSnapArea dropArea = draggable.currentSnappable.GetComponent<SolutionSnapArea>();
+		SolutionBlock solutionBlock = obj.GetComponent<SolutionBlock> ();
+		SolutionSnapArea dropArea = draggable.currentSnappable.GetComponent<SolutionSnapArea> ();
         
-      	int index = dropArea.Index;
-      	object guess = solutionBlock.data;
+		int index = dropArea.Index;
+		object guess = solutionBlock.data;
   	
         
-        if (snappable.isAlreadyOccupied) {
+		if (snappable.isAlreadyOccupied) {
             
-            object temp = slnManager.GetGuess(index);
-            draggable.Reset();
-            slnManager.SetGuess(index, temp);
+			object temp = slnManager.GetGuess (index);
+			draggable.Reset ();
+			slnManager.SetGuess (index, temp);
              
 
-        }
-        else {
-            slnManager.SetGuess(index, guess);
+		} else {
+			slnManager.SetGuess (index, guess);
 
-        }
+		}
         
 
-    }
+	}
 
    
-    /// <summary>
-    /// Called when a the guess (SolutionBlock) exits a SolutionInput either
-    /// by the player dragging it out or being moved by code. 
-    /// </summary>
-    void GuessVacate(GameObject obj) {
-        Draggable d = obj.GetComponent<Draggable>();
-        SolutionBlock solutionBlock = obj.GetComponent<SolutionBlock>();
+	/// <summary>
+	/// Called when a the guess (SolutionBlock) exits a SolutionInput either
+	/// by the player dragging it out or being moved by code. 
+	/// </summary>
+	void GuessVacate (GameObject obj)
+	{
+		Draggable d = obj.GetComponent<Draggable> ();
+		SolutionBlock solutionBlock = obj.GetComponent<SolutionBlock> ();
 
-        SolutionSnapArea dropArea = d.currentSnappable.GetComponent<SolutionSnapArea>();
-        int index = dropArea.Index;
+		SolutionSnapArea dropArea = d.currentSnappable.GetComponent<SolutionSnapArea> ();
+		int index = dropArea.Index;
 
-        object vacatingGuess = solutionBlock.data;
+		object vacatingGuess = solutionBlock.data;
         
-        object currentGuess = slnManager.GetGuess(index);
+		object currentGuess = slnManager.GetGuess (index);
             
-        // Guess vacating because snaparea already occupied so
-        // keep current guess.
-        if (currentGuess != vacatingGuess)
-            return;
+		// Guess vacating because snaparea already occupied so
+		// keep current guess.
+		if (currentGuess != vacatingGuess)
+			return;
 
-        // Otherwise it means that the snap area will be empty
-        // after the guess vacates so set it to NOT_GUESSED
-        slnManager.ResetGuess(index);
+		// Otherwise it means that the snap area will be empty
+		// after the guess vacates so set it to NOT_GUESSED
+		slnManager.ResetGuess (index);
         
-    }
+	}
 	
 	
 	/// <summary>
 	/// Move the guess blocks back to their starting positions and
 	/// reset the guesses internally back to "NOT_GUESSED".
 	/// </summary>
-	void ResetGuesses(){
-		GetComponentInChildren<SolutionBlocks>().Reset();
+	void ResetGuesses ()
+	{
+		GetComponentInChildren<SolutionBlocks> ().Reset ();
 		// Might be belt and suspenders to do this as well
 		// as the prevous reset since the blocks moving outside of the
 		// input blocks will cause a reset event.
-		slnManager.ResetGuesses();
+		slnManager.ResetGuesses ();
 	}
 	
 
 	/// <summary>
 	/// EventManager callback.
 	/// </summary>
-    public bool HandleEvent(IEvent evnt) {
-        string eventName = evnt.GetName();
-        object eventData = evnt.GetData();
+	public bool HandleEvent (IEvent evnt)
+	{
+		string eventName = evnt.GetName ();
+		object eventData = evnt.GetData ();
 
-        if (eventName == onSnapName) {
+		if (eventName == onSnapName) {
 
-            GuessDropped(eventData as GameObject);
-        }
-        else if (eventName == onMoveName) {
-            // ignore
-        }
-        else if (eventName == snapOnEnterName) {
-            // ignore                                                                  e
-        }
-        else if (eventName == snapOnExitName) {
+			GuessDropped (eventData as GameObject);
+		} else if (eventName == onMoveName) {
+			// ignore
+		} else if (eventName == snapOnEnterName) {
+			// ignore                                                                  e
+		} else if (eventName == snapOnExitName) {
   
-            GuessVacate(eventData as GameObject);
+			GuessVacate (eventData as GameObject);
 
-        }
-        else {
-            Debug.LogWarning("Unexpected Event in BC: " + eventName);
-            return false;
-        }
-        return true;
-    }
+		} else {
+			Debug.LogWarning ("Unexpected Event in BC: " + eventName);
+			return false;
+		}
+		return true;
+	}
 	
 }
--- Assets/Code/Utilities/EventManager.cs
+++ Assets/Code/Utilities/EventManager.cs
@@ -3,40 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-
-public interface IEventListener {
-    bool HandleEvent(IEvent evt);
+public interface IEventListener
+{
+	bool HandleEvent (IEvent evt);
 }
 
+public interface IEvent
+{
+	string GetName ();
 
-public interface IEvent {
-    string GetName();
-    object GetData();
+	object GetData ();
 }
 
 /// <summary>
 /// Base Event Object
 /// </summary>
-public class EventObject : IEvent {
-    private string eventName;
-    private object eventData;
+public class EventObject : IEvent
+{
+	private string eventName;
+	private object eventData;
 	
-    public EventObject(object data) {
-        eventName = this.GetType().ToString();
-        eventData = data;
-    }
+	public EventObject (object data)
+	{
+		eventName = this.GetType ().ToString ();
+		eventData = data;
+	}
 
-    public EventObject() {
-        eventName = this.GetType().ToString();
-        eventData = null;
-    }
+	public EventObject ()
+	{
+		eventName = this.GetType ().ToString ();
+		eventData = null;
+	}
 
-    public virtual string GetName() {
-        return eventName;
-    }
-    public virtual object GetData() {
-        return eventData;
-    }
+	public virtual string GetName ()
+	{
+		return eventName;
+	}
+
+	public virtual object GetData ()
+	{
+		return eventData;
+	}
 
 }
 
@@ -47,95 +54,97 @@ public class EventObject : IEvent {
 ///  1. The primary extention for this class would be to make 
 ///     the eventListenerMap handle multiple listeners for the same
 ///     event type. This is not needed for the current usage.
-public class EventManager : MonoBehaviour {
+public class EventManager : MonoBehaviour
+{
 
-    private static EventManager singleton = null;
+	private static EventManager singleton = null;
 
-    // event-name => event listerer class instance
-    private Dictionary<string, IEventListener> eventListenerMap;
+	// event-name => event listerer class instance
+	private Dictionary<string, IEventListener> eventListenerMap;
 
-
-    public static bool isReady {
-        get {
-            return singleton != null;
-        }
-    }
+	public static bool isReady {
+		get {
+			return singleton != null;
+		}
+	}
 	
 	/// <summary>
 	/// Get the EventManager singleton safely.
 	/// </summary>
-    public static EventManager instance {
-        get {
-            if (!EventManager.isReady) {
-                // Singleton is unitialized so dynamically make a new empty
-                // gameobject to which we we can attach the EventManager script.
-                // This allows for objects to use the EventManager without worrying
-                // about if it is initialized and without having a static instance 
-                // always hanging around.
-                GameObject attachmentObj = new GameObject("EventManager");
+	public static EventManager instance {
+		get {
+			if (!EventManager.isReady) {
+				// Singleton is unitialized so dynamically make a new empty
+				// gameobject to which we we can attach the EventManager script.
+				// This allows for objects to use the EventManager without worrying
+				// about if it is initialized and without having a static instance 
+				// always hanging around.
+				GameObject attachmentObj = new GameObject ("EventManager");
 
-                singleton = attachmentObj.AddComponent<EventManager>();
-            }
-            // Return the new instance of EventManager that is attached
-            // to the game object.
-            return singleton;
-        }
-    }
+				singleton = attachmentObj.AddComponent<EventManager> ();
+			}
+			// Return the new instance of EventManager that is attached
+			// to the game object.
+			return singleton;
+		}
+	}
 
-    // Awake is called when the base is initialized. Start() 
-    // is called before the first update. Initializing the manager 
-    // makes sure the singleton is initialized before events start 
-    // happening since other objects may be "started" before the eventmanager.
-    void Awake() {
-        eventListenerMap = new Dictionary<string, IEventListener>();
-    }
+	// Awake is called when the base is initialized. Start() 
+	// is called before the first update. Initializing the manager 
+	// makes sure the singleton is initialized before events start 
+	// happening since other objects may be "started" before the eventmanager.
+	void Awake ()
+	{
+		eventListenerMap = new Dictionary<string, IEventListener> ();
+	}
 
+	public bool RegisterListener (IEventListener listener, string eventName)
+	{
 
-    public bool RegisterListener(IEventListener listener, string eventName) {
+		if (listener == null || eventName == null) {
+			Debug.LogError ("RegisterLisener(): failed due to listener or event == null");
+			return false;
+		}
+		if (eventListenerMap.ContainsKey (eventName)) {
+			Debug.LogWarning ("Overwriting listener for " + eventName);
+		}
 
-        if (listener == null || eventName == null) {
-            Debug.LogError("RegisterLisener(): failed due to listener or event == null");
-            return false;
-        }
-        if (eventListenerMap.ContainsKey(eventName)) {
-            Debug.LogWarning("Overwriting listener for " + eventName);
-        }
+		// One listener type per event! 
+		eventListenerMap [eventName] = listener;
+		return true;
+	}
 
-        // One listener type per event! 
-        eventListenerMap[eventName] = listener;
-        return true;
-    }
-
-
-    bool HasListener(IEvent evnt) {
-        return eventListenerMap.ContainsKey(evnt.GetName());
-    }
+	bool HasListener (IEvent evnt)
+	{
+		return eventListenerMap.ContainsKey (evnt.GetName ());
+	}
 	
-    /// <summary>
-    /// If there is a listener for evnt then relay the event to 
-    /// that listener immediately. 
-    /// </summary>
-	public bool RelayEvent(IEvent evnt) {
-        if (!HasListener(evnt)) {
-            Debug.LogWarning("RelayEvent(): No listener for " + evnt.GetName());
-            return false;
-        }
-        DispatchEvent(evnt);
-        return true;
-    }
+	/// <summary>
+	/// If there is a listener for evnt then relay the event to 
+	/// that listener immediately. 
+	/// </summary>
+	public bool RelayEvent (IEvent evnt)
+	{
+		if (!HasListener (evnt)) {
+			Debug.LogWarning ("RelayEvent(): No listener for " + evnt.GetName ());
+			return false;
+		}
+		DispatchEvent (evnt);
+		return true;
+	}
 
 
-    // Calls the listener's HandleEvent() methods for the event
-    void DispatchEvent(IEvent evnt) {
-        string eventName = evnt.GetName();
-        IEventListener listener = eventListenerMap[eventName];
-        listener.HandleEvent(evnt);
-    }
+	// Calls the listener's HandleEvent() methods for the event
+	void DispatchEvent (IEvent evnt)
+	{
+		string eventName = evnt.GetName ();
+		IEventListener listener = eventListenerMap [eventName];
+		listener.HandleEvent (evnt);
+	}
 
-
-
-    public void OnApplicationQuit() {
-        eventListenerMap.Clear();
-        singleton = null;
-    }
+	public void OnApplicationQuit ()
+	{
+		eventListenerMap.Clear ();
+		singleton = null;
+	}
 }
