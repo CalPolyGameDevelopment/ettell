4ce74abaf5c57fa731a0eb4950a6b6bca59f2260
Dillon Hicks <chronodynamic@gmail.com>
2012-11-23 16:16:32 -0600

    Deleting redundant code and setting bounds object parent.

--- Assets/Bulls And Cleaots/Mechanics/BlockManager.cs
+++ Assets/Bulls And Cleaots/Mechanics/BlockManager.cs
@@ -7,76 +7,77 @@ using System.Linq;
 namespace BullsAndCleots.Mechanics {
 
 public class BlockManager : MonoBehaviour {
- 
-    
+
+
 	public GameObject blockPrefab;
 	public Transform startTransform;
-	public GameObject bounds; 
-		
+	public GameObject bounds;
+
 	private List<GameObject> blocks;
 	// For some reason how I setup the scene makes the textures upside down
 	// so flip them.
 	private static Vector3 orientation = new Vector3(0, 0, 180);
 	private	List<Material[]> choices = null;
-    
+
 	public List<List<Material>> Choices {
 		set {
 			if (choices != null){
 				choices.Clear();
 			}
 			choices = new List<Material[]>();
-				
+
 			foreach(List<Material> mats in value){
-				choices.Add (mats.ToArray());			
+				choices.Add (mats.ToArray());
 			}
 		}
 	}
- 
+
 
 	void layoutRow(int rowIndex, Rect bnds, float blockHeight, List<GameObject> row, int totalRows){
 		int rowCount = row.Count;
-		
+
 		// Should be a constant for a given row.
 		float z = ((bnds.height) / (totalRows+ 1)) * rowIndex + bnds.yMax ;
 		int index = 0;
 		foreach(GameObject block in row){
 			float x = ((bnds.width - blockHeight) / rowCount) * index + bnds.xMax;
-				
+
 			block.transform.position = new Vector3(x, startTransform.position.y, z);
 			index++;
 		}
 	}
-     
-    
+
+
 	// Use this for initialization
 	void Start() {
-			
+
 		bounds = Instantiate(bounds) as GameObject;
+		bounds.transform.parent = gameObject.transform;
+
 		Rect layoutRect = bounds.GetComponent<BoundingBox>().Bounds;
-			
+
 		blocks = new List<GameObject>();
 		int rowIndex = 0;
 		foreach (Material[] mats in choices) {
 			List<GameObject> row = new List<GameObject>();
 			foreach(Material mat in mats){
-					
+
 				GameObject newBlock = Instantiate(blockPrefab) as GameObject;
 				newBlock.transform.parent = gameObject.transform;
-            
 				GameBlock solutionBlock = newBlock.GetComponent<GameBlock>();
-				newBlock.transform.parent = gameObject.transform;
+
 
 				newBlock.renderer.material = mat;
 				solutionBlock.data = mat;
 				blocks.Add(newBlock);
             	row.Add(newBlock);
 				}
-					
+
 			layoutRow(rowIndex, layoutRect, 1.0f, row, choices.Count);
 			rowIndex++;
 			row.Clear();
 		}
-        
+
 		blocks.ForEach(b => b.transform.Rotate(orientation));
 	}
 
@@ -91,5 +92,5 @@ public class BlockManager : MonoBehaviour {
 	}
 
 }
-	
+
 }
