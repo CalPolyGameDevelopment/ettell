45255802193c866f8008b9345da93d37ad2ecdac
Dillon <chronodynamic@gmail.com>
2012-11-23 01:23:18 -0600

    Layout code for the BC game pieces.
    Just tries to equally fill the specified area in a sane manner.

--- Assets/Bulls And Cleaots/Mechanics/BlockManager.cs
+++ Assets/Bulls And Cleaots/Mechanics/BlockManager.cs
@@ -11,54 +11,73 @@ public class BlockManager : MonoBehaviour {
     
 	public GameObject blockPrefab;
 	public Transform startTransform;
+	public GameObject bounds; 
+		
 	private List<GameObject> blocks;
-	private float layoutSpacing = 1.2f;
 	// For some reason how I setup the scene makes the textures upside down
 	// so flip them.
 	private static Vector3 orientation = new Vector3(0, 0, 180);
-	private Material[] choices;
-	private int count;
+	private	List<Material[]> choices = null;
     
-	public Material[] Choices {
+	public List<List<Material>> Choices {
 		set {
-			count = value.GetLength(0);
-			choices = new Material[count];
-			Array.Copy(value, choices, count);
+			if (choices != null){
+				choices.Clear();
+			}
+			choices = new List<Material[]>();
+				
+			foreach(List<Material> mats in value){
+				choices.Add (mats.ToArray());			
+			}
 		}
 	}
  
+
+	void layoutRow(int rowIndex, Rect bnds, float blockHeight, List<GameObject> row, int totalRows){
+		int rowCount = row.Count;
+		
+		// Should be a constant for a given row.
+		float z = ((bnds.height) / (totalRows+ 1)) * rowIndex + bnds.yMax ;
+		int index = 0;
+		foreach(GameObject block in row){
+			float x = ((bnds.width - blockHeight) / rowCount) * index + bnds.xMax;
+				
+			block.transform.position = new Vector3(x, startTransform.position.y, z);
+			index++;
+		}
+	}
      
     
 	// Use this for initialization
 	void Start() {
-
-		Vector3 pos = startTransform.position;
-
-		Func<Vector3, Vector3> LayoutObject = oldPos => new Vector3(
-            oldPos.x + layoutSpacing, oldPos.y, oldPos.z);
-
+			
+		bounds = Instantiate(bounds) as GameObject;
+		Rect layoutRect = bounds.GetComponent<BoundingBox>().Bounds;
+			
 		blocks = new List<GameObject>();
-        
-		foreach (Material mat in choices) {
-			GameObject newBlock = Instantiate(blockPrefab) as GameObject;
-			newBlock.transform.parent = gameObject.transform;
+		int rowIndex = 0;
+		foreach (Material[] mats in choices) {
+			List<GameObject> row = new List<GameObject>();
+			foreach(Material mat in mats){
+					
+				GameObject newBlock = Instantiate(blockPrefab) as GameObject;
+				newBlock.transform.parent = gameObject.transform;
             
-			GameBlock solutionBlock = newBlock.GetComponent<GameBlock>();
-			newBlock.transform.parent = gameObject.transform;
-			newBlock.transform.position = LayoutObject(pos);
-			newBlock.transform.Rotate(orientation);
+				GameBlock solutionBlock = newBlock.GetComponent<GameBlock>();
+				newBlock.transform.parent = gameObject.transform;
 
-			newBlock.renderer.material = mat;
-            
-			solutionBlock.data = mat;
-			blocks.Add(newBlock);
-            
-			pos = newBlock.transform.position;
+				newBlock.renderer.material = mat;
+				solutionBlock.data = mat;
+				blocks.Add(newBlock);
+            	row.Add(newBlock);
+				}
+					
+			layoutRow(rowIndex, layoutRect, 1.0f, row, choices.Count);
+			rowIndex++;
+			row.Clear();
 		}
-
-
         
-
+		blocks.ForEach(b => b.transform.Rotate(orientation));
 	}
 
 	public void Reset() {
---
+++ Assets/Bulls And Cleaots/Mechanics/BoundingBox.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Bounding box.
+///  
+/// Assumes XZ plane projection centered on the parent GameObjects transform.
+/// </summary>
+public class BoundingBox : MonoBehaviour {
+
+	public float width;
+	public float height;
+	private Color retinaBurningColor = new Color(255, 0, 255);
+	
+	Vector3 Size {
+		get{
+			return new Vector3(width, 0, height);			
+		}
+	}
+	
+	public Rect Bounds{
+		get {
+			float left, top;
+			left = transform.position.x - (width/2.0F);
+			top = transform.position.z - (height/2.0F);
+			return new Rect(left, top, width, height);
+		}
+	}
+	
+	void OnDrawGizmos(){
+		Gizmos.color = retinaBurningColor;
+		Gizmos.DrawWireCube(transform.position, Size);
+	}
+}
